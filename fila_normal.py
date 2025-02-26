06dfc1ba4c3741d78ec78a8c516d70d34143dc09
Manu Correa <thsilvar@gmail.com>
2025-02-26 14:05:29 -0300

    Construindo nossa classe

--- fila_normal.py
+++ fila_normal.py
@@ -7,4 +7,19 @@ class FilaNormal:
     def gerar_senha_atual(self) -> None:
         self.senha_atual = f"NM{self.codigo}"
 
-    
+    def reseta_fila(self) -> None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
+
+    def atualiza_fila(self) -> None:
+        self.reseta_fila()
+        self.gerar_senha_atual()
+        self.fila.append(self.senha_atual)
+        
+
+    def chamar_cliente(self, caixa: int) -> str:
+        cliente_atual:str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return (f"Cliente atual: {cliente_atual} - Caixa: {caixa}")
---
+++ main.py
@@ -0,0 +1,10 @@
+from fila_normal import FilaNormal
+
+fila_teste = FilaNormal()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+
+print(fila_teste.chamar_cliente(5))
+print(fila_teste.chamar_cliente(5))
+print(fila_teste.chamar_cliente(15))
