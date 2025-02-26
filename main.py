4e142140660d651f2a371ddf7cd898da063fa39d
Manu Correa <thsilvar@gmail.com>
2025-02-26 14:35:35 -0300

    O poder do PEP 8

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fila_normal import FilaNormal
+from fila_prioritaria import FilaPrioritaria
 
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
+#fila_teste = FilaNormal()
+#fila_teste.atualiza_fila()
+#fila_teste.atualiza_fila()
+#fila_teste.atualiza_fila()
+
+#print(fila_teste.chamar_cliente(5))
+#print(fila_teste.chamar_cliente(5))
+#print(fila_teste.chamar_cliente(15))
+
+fila_teste = FilaPrioritaria()
 fila_teste.atualiza_fila()
 fila_teste.atualiza_fila()
 
-print(fila_teste.chamar_cliente(5))
 print(fila_teste.chamar_cliente(5))
 print(fila_teste.chamar_cliente(15))
+print(fila_teste.estatistica('10/01/2025', 198, 'detail'))
